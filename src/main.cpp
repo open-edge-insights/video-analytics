5ba3864b7daec77c20e6b97191a989f4d6339ffe
vka <varalakshmi.ka@intel.com>
2019-11-14 22:31:37 +0530

    VideoIngestion, VideoAnalytics: Added dynamic config update
    Changes:
    1. For VI, Moved all initialization parts to constructor and main.cpp
    2. Added dynamic config update for both VI and VA
    
    Signed-off-by: vka <varalakshmi.ka@intel.com>
    Signed-off-by: Vinod K B <vinod.k.b@intel.com>

--- src/main.cpp
+++ src/main.cpp
@@ -27,26 +27,56 @@
 #include <unistd.h>
 #include <condition_variable>
 #include "eis/va/video_analytics.h"
+#include <mutex>
+
+#define MAX_CONFIG_KEY_LENGTH 40
 
 using namespace eis::va;
 using namespace eis::utils;
+
+static VideoAnalytics* g_va = NULL;
+static EnvConfig* g_config = NULL;
+static char* g_va_config = NULL;
+static std::condition_variable g_err_cv;
 
 void usage(const char* name) {
     printf("Usage: %s \n", name);
 }
 
+void va_initialize(char* va_config){
+    if(g_va) {
+        delete g_va;
+    }
+    if(!g_config) {
+        g_config = new EnvConfig();
+    }
+
+    g_va = new VideoAnalytics(g_err_cv, g_config, va_config);
+    g_va->start();
+}
+
+void on_change_config_callback(char* key, char* va_config){
+    if(strcmp(g_va_config, va_config)){
+        va_initialize(va_config);
+    }
+}
+
 int main(int argc, char** argv) {
-    VideoAnalytics* va = NULL;
-    char* str_log_level = NULL;
-    EnvConfig* config = NULL;
+    config_mgr_t* config_mgr = NULL;
     log_lvl_t log_level = LOG_LVL_ERROR; // default log level is `ERROR`
     try {
         if(argc >= 2) {
             usage(argv[0]);
             return -1;
         }
-        config = new EnvConfig();
+        char* str_log_level = NULL;
+        g_config = new EnvConfig();
+        log_lvl_t log_level = LOG_LVL_ERROR;
+
         str_log_level = getenv("C_LOG_LEVEL");
+        if(str_log_level == NULL) {
+            throw "\"C_LOG_LEVEL\" env not set";
+        }
 
         if(strcmp(str_log_level, "DEBUG") == 0) {
             log_level = LOG_LVL_DEBUG;
@@ -59,26 +89,36 @@ int main(int argc, char** argv) {
         }
         set_log_level(log_level);
 
-        std::condition_variable err_cv;
-        va = new VideoAnalytics(err_cv, config);
-        va->start();
+        std::string m_app_name = getenv("AppName");
+        config_mgr_t* config_mgr = g_config->get_config_mgr_client();
+
+        // Get the configuration from the configuration manager
+        char config_key[MAX_CONFIG_KEY_LENGTH];
+        sprintf(config_key, "/%s/config", m_app_name.c_str());
+        g_va_config = config_mgr->get_config(config_key);
+        LOG_DEBUG("App config: %s", g_va_config);
+
+        LOG_DEBUG("Registering watch on config key: %s", config_key);
+        config_mgr->register_watch_key(config_key, on_change_config_callback);
+
+        va_initialize(g_va_config);
+
         std::mutex mtx;
         std::unique_lock<std::mutex> lk(mtx);
-        err_cv.wait(lk);
-        delete va;
-        delete config;
-    } catch(const std::exception& e) {
+        g_err_cv.wait(lk);
+
+        delete g_va;
+        delete g_config;
+        return -1;
+    }catch(const std::exception& e) {
         LOG_ERROR("Exception occurred: %s", e.what());
-        if(va != NULL) {
-            delete va;
+        if(g_va) {
+            delete g_va;
         }
-        if(config != NULL) {
-            delete config;
+        if(g_config) {
+            delete g_config;
         }
+        config_mgr_config_destroy(config_mgr);
         return -1;
     }
-    return 0;
 }
-
-
-
