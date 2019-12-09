a71f9d1252ea36657c4900e3fca41107e4f0f6bf
Vishwas R <vishwas.r@intel.com>
2019-12-09 23:28:57 +0530

    VideoAnalytics: Fix for enabling worker safety usecase
    Changes:
    1. Added libcpu_extension.so path to LD_LIBRARY_PATH
    
    Signed-off-by: Vishwas R <vishwas.r@intel.com>

--- va_classifier_start.sh
+++ va_classifier_start.sh
@@ -5,4 +5,7 @@ source /opt/intel/openvino/bin/setupvars.sh
 udevadm control --reload-rules
 udevadm trigger
 
+# Adding path of libcpu_extension.so to LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GO_WORK_DIR/common/udfs/native/build/ie_cpu_extension
+
 ./VideoAnalytics/build/video-analytics
